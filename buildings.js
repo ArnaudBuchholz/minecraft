e3b744260ce55348a7b61b80ab0569b1d68ed988
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-08-20 23:19:20 -0400

    List content of buildings folder

---
+++ buildings.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const { readdir } = require('fs')
+const readdirAsync = require('util').promisify(readdir)
+const { join } = require('path')
+
+module.exports = async (request, response) => {
+  const names = await readdirAsync(join(__dirname, 'data/buildings'))
+  response.writeHead(200, {
+    'content-type': 'application/json'
+  })
+  response.end(JSON.stringify(names))
+}
