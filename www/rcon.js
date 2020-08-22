e6e969ac89b76d49740dc11f4c4d95c67e21b5d6
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-08-21 23:31:28 -0400

    Asynchronous FIFO queue

---
+++ www/rcon.js
@@ -0,0 +1,35 @@
+'use strict'
+
+function rcon (cmd) {
+  let promise
+  const { queue, execute, done } = rcon
+  const { length } = queue
+  if (!length) {
+    promise = rcon.execute(cmd)
+  } else {
+    const last = queue[length - 1]
+    promise = last.then(() => execute(cmd))
+  }
+  queue.push(promise.then(done, done))
+  return promise
+}
+
+rcon.queue = []
+
+rcon.done = () => {
+  rcon.queue.shift()
+}
+
+rcon.execute = async cmd => {
+  const response = await fetch('/rcon', {
+    method: 'POST',
+    headers: {
+      'content-type': 'text/plain'
+    },
+    body: cmd
+  })
+  if (!response.ok) {
+    throw response.statusText
+  }
+  return response.text()
+}
