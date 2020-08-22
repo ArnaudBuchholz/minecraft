e3b7e19b3a1409f6cfc3ff7351919d76afa40730
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-08-21 23:31:35 -0400

    Linter fix

--- data/buildings/pillar.js
+++ data/buildings/pillar.js
@@ -4,12 +4,12 @@ const lantern = 'soul_lantern[hanging=true]'
 const fire = 'soul_campfire[lit=true]'
 
 builder('pillar', build => {
-  for (let y = -1; y < 5; ++h) {
+  for (let y = -1; y < 5; ++y) {
     build.block(0, y, 0, stone)
   }
   build.block(0, 5, 0, fire)
   const lanterns = [{ x: +1, z: 0 }, { x: -1, z: 0 }, { x: 0, z: +1 }, { x: 0, z: -1 }]
-  lanterns.forEach(({x, z}) => {
+  lanterns.forEach(({ x, z }) => {
     build.block(x, 4, z, support)
     build.block(x, 3, z, lantern)
   })
