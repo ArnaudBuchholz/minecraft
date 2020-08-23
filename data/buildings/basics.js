6f9e0d2fe56cce57e7f35467c87d432e7ad250e7
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-08-23 12:58:53 -0400

    Improve readability

--- data/buildings/basics.js
+++ data/buildings/basics.js
@@ -1,13 +1,12 @@
+const floor = 'netherite_block'
 const stone = 'chiseled_polished_blackstone'
 const support = 'polished_blackstone_brick_wall'
-const lantern = {
-  $type: 'soul_lantern',
-  hanging: true
-}
-const fire = {
-  $type: 'soul_campfire',
-  lit: true
-}
+const stairs = 'polished_blackstone_stairs'
+const lantern = { $type: 'soul_lantern', hanging: true }
+const fire = { $type: 'soul_campfire', lit: true }
+
+const front = { S: 'south', W: 'west', N: 'north', E: 'east' }
+const back = { S: 'north', W: 'east', N: 'south', E: 'west' }
 
 builder('pillar', build => {
   for (let y = -1; y < 5; ++y) {
@@ -22,28 +21,22 @@ builder('pillar', build => {
 })
 
 builder('teleport temple', build => {
-  
+  // current position is the teleport target
+  // platform
+  let x, y, z
+  for (z = -2; z < 7; ++z) {
+    for (x = -2; x < 3; ++x) {
+      build.block(x, -1, z, floor)
+    }
+  }
+  build.block(0, -1, 0, stone)
+  build.block(-2, 0, -2, { $type: stairs, facing: back })
+  build.block(2, 0, -2, { $type: stairs, facing: back })
 })
 
 builder('test', build => {
-  build.block(0, 0, 2, {
-    $type: 'purpur_stairs',
-    facing: {
-      S: 'south',
-      W: 'west',
-      N: 'north',
-      E: 'east'
-    }
-  })
-  build.block(1, 0, 2, {
-    $type: 'barrel',
-    facing: {
-      S: 'north',
-      W: 'east',
-      N: 'south',
-      E: 'west'
-    }
-  })
+  build.block(0, 0, 2, { $type: 'purpur_stairs', facing: front })
+  build.block(1, 0, 2, { $type: 'barrel', facing: back })
   build.block(-1, 0, 2, {
     $type: 'dark_oak_leaves',
     persistent: true
