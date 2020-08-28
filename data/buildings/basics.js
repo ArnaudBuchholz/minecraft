0b1f062f37bafbcf1e28c0f781d0aac380450818
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-08-27 23:21:03 -0400

    isolate constants

--- data/buildings/basics.js
+++ data/buildings/basics.js
@@ -1,21 +1,4 @@
-const air = 'air'
-const floor = 'netherite_block'
-const stone = 'chiseled_polished_blackstone'
-const bricks = 'polished_blackstone_bricks'
-const slab = 'polished_blackstone_slab'
-const support = 'polished_blackstone_brick_wall'
-const stairs = 'polished_blackstone_stairs'
-const arrow = 'magenta_glazed_terracotta'
-const lantern = { $type: 'soul_lantern', hanging: true }
-const walltorch = 'soul_wall_torch'
-const fire = { $type: 'soul_campfire', lit: true }
-const command = 'command_block'
-const pressurePlate = 'polished_blackstone_pressure_plate'
-
-const front = { S: 'south', W: 'west', N: 'north', E: 'east' }
-const left = { S: 'east', W: 'south', N: 'west', E: 'north' }
-const right = { S: 'west', W: 'north', N: 'east', E: 'south' }
-const back = { S: 'north', W: 'east', N: 'south', E: 'west' }
+'use strict'
 
 builder('pillar', build => {
   build.fill(0, -1, 0, 0, 4, 0, stone)
