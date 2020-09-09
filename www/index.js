9989126354718d2df0af2c657689e0259264fd15
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-09-09 09:26:26 -0400

    Isolate features

--- www/index.js
+++ www/index.js
@@ -1,11 +1,13 @@
 'use strict'
 
-import { byId } from './tools.js'
+import { byId, option } from './tools.js'
 import data from './data.js'
 import Builder from './Builder.js'
 import xyzHelper from './xyz.js'
 import rcon from './rcon.js'
 
+const facing = () => option(byId('facing')).value
+const user = () => option(byId('users')).value
 const xyz = xyzHelper(() => byId('xyz').value, (value, isProtected) => {
   const input = byId('xyz')
   input.value = value
@@ -16,30 +18,16 @@ const xyz = xyzHelper(() => byId('xyz').value, (value, isProtected) => {
   }
 })
 
-function facing () {
-  const select = byId('facing')
-  return select.options[select.selectedIndex].value
-}
-
-const option = (value, label = value) => {
-  const element = document.createElement('option')
-  element.value = value
-  element.appendChild(document.createTextNode(label))
-  return element
-}
-
 const actions = {
   users: async () => {
     const output = await rcon(`list`)
     const users = byId('users')
     users.innerHTML = ''
-    output.split('online:')[1].replace(/\w+/ig, name => {
-      users.appendChild(option(name, name))
-    })
+    output.split('online:')[1].replace(/\w+/ig, name => users.appendChild(option(name)))
   },
 
   position: async () => {
-    const output = await rcon(`execute at ${user} run teleport ${user} ~ ~ ~`)
+    const output = await rcon(`execute at ${user()} run teleport ${user()} ~ ~ ~`)
     if (!output) {
       xyz.set()
       return
@@ -52,7 +40,7 @@ const actions = {
     const { x, y, z } = xyz()
     const rotation = { N: -180, S: 0, W: 90, E: -90 }[facing()]
     if (x !== undefined) {
-      rcon(`teleport ${user} ${x} ${y} ${z} ${rotation} 0`)
+      rcon(`teleport ${user()} ${x} ${y} ${z} ${rotation} 0`)
     }
   },
 
@@ -71,8 +59,10 @@ const actions = {
       return
     }
     const build = new Builder({ x, y, z }, facing())
+/*
     const buildings = byId('buildings')
-    builder[buildings.options[buildings.selectedIndex].value](build)
+    builder[option(buildings).value](build)
+*/
     byId('area-to-erase').value = build.area
   },
 
@@ -90,7 +80,7 @@ document.addEventListener('click', event => {
     if (target.dataset.action) {
       actions[target.dataset.action]()
     } else if (target.dataset.cmd) {
-      rcon(target.dataset.cmd.replace(/\$\{user\}/g, user)).then(console.log)
+      rcon(target.dataset.cmd.replace(/\$\{user\(\)\}/g, user())).then(console.log)
     }
   }
 })
@@ -105,8 +95,7 @@ window.addEventListener('load', async () => {
     byId('shortcuts').appendChild(option(shortcut.xyz, shortcut.label))
   })
   byId('shortcuts').addEventListener('change', function () {
-    const option = this.options[this.selectedIndex]
-    xyz.set(option.value)
+    xyz.set(option(this).value)
   })
   const buildings = await data('buildings/.')
   buildings
@@ -119,39 +108,9 @@ window.addEventListener('load', async () => {
     })
 })
 
+/*
 function builder (label, factory) {
   builder[label] = factory
   byId('buildings').appendChild(option(label))
 }
-
-function setCompass (edges, vertical, horizontal) {
-  const ids = ['up', 'left', 'down', 'right']
-  ids.forEach((id, index) => {
-    byId(`compass-${id}`).innerHTML = edges.charAt(index)
-  })
-  byId('compass-vertical').innerHTML = vertical
-  byId('compass-horizontal').innerHTML = horizontal
-}
-
-const compass = {
-  S: () => setCompass('SWNE', '+Z', '+X'),
-  W: () => setCompass('WNES', '+X', '-Z'),
-  N: () => setCompass('NESW', '-Z', '-X'),
-  E: () => setCompass('ESWN', '-X', '+Z')
-}
-
-byId('facing').addEventListener('change', () => {
-  compass[facing()]()
-})
-
-const alphabet = '\u311A\u3105\u3118\u3109\u311C\u3108\u310D\u310F\u3127\u3110\u310E\u310C\u3107\u310B\u311B\u3106\u3111\u3116\u3119\u310A\u3128\u3125\u3120\u3112\u312D\u3117'
-
-byId('message').addEventListener('change', function () {
-  byId('encrypted').value = this.value.toUpperCase().split('').map(char => {
-    const index = char.charCodeAt(0) - 65
-    if (index >= 0 && index < 27) {
-      return alphabet[index]
-    }
-    return ' '
-  }).join('')
-})
+*/
