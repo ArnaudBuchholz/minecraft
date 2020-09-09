04b37eaac1c9fcae82800b5d2781487c40323158
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-09-09 09:26:18 -0400

    option

--- www/tools.js
+++ www/tools.js
@@ -1,3 +1,13 @@
 'use strict'
 
 export const byId = document.getElementById.bind(document)
+
+export const option = (value, label = value) => {
+  if (value.selectedIndex !== undefined) {
+    return value.options[value.selectedIndex]
+  }
+  const element = document.createElement('option')
+  element.value = value
+  element.appendChild(document.createTextNode(label))
+  return element
+}
