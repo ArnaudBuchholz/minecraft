bd02b28148aba4c2f2bbbed7c535dc8bdf972207
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-09-15 07:16:18 -0400

    Better decoupling

--- www/builders.js
+++ www/builders.js
@@ -1,12 +1,18 @@
 'use strict'
 
-const builders = {}
+const builders = []
 
 export function declare (label, factory) {
-  builders[label] = factory
-  document.body.dispatchEvent(new CustomEvent("builder", {
+  const id = builders.length
+  builders.push(factory)
+  document.body.dispatchEvent(new CustomEvent("declare-builder", {
     detail: {
+      id,
       label
     }
   }))
+}
+
+export function execute (id, build) {
+  builders[id](build)
 }
